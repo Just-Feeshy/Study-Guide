9d1bd71f416704f112861b16cf1ebed00252045d
Feeshy Fesh <58647349+Just-Feeshy@users.noreply.github.com>
2022-04-19 19:26:18 +0000

    Made Reversed Array.

--- Main.java
+++ Main.java
@@ -12,17 +12,21 @@ class Main {
     public static void main(String[] args) {
         DoStuff n = new DoStuff();
         MyStuff y = new MyStuff();
-        
-        //System.out.println(n.addingUpTo(new int[] {1, 4, 5, 6}, 2));
 
-        System.out.println(n.notFun(7));
-        System.out.println(n.remember(5));
-        n.build(51);
-        n.wow("pacman", 2);
-
+        int[] arr = {5, 7, 2, 1, 9, 3};
         
-        System.out.println("\nrows:");
-        n.test1(7);
-        //System.out.println(y.getTotalFromArray(new int[] {76, 64, 1, 7, 54, 7, 9, 10, 1, 0, 7, 8}));
+        int j = 0;
+        int k = arr.length - 1;
+
+        while(j < k) {
+            int x = arr[j];
+            arr[j] = arr[k];
+            arr[k] = x;
+
+            j++;
+            k--;
+        }
+
+        System.out.println(arr[0]);
     }
 }
