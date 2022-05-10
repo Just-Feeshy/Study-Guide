c2a4c3b0e9d986a0f2dc258f3f1c1fb8325ad910
Feeshy Fesh <58647349+Just-Feeshy@users.noreply.github.com>
2022-05-10 17:18:09 +0000

    Finds current file in subdirectories.

---
+++ Locator.java
@@ -0,0 +1,38 @@
+import java.io.File;
+
+/*
+* Task: Locate for file in both the ASSIGNED directory and sub directories and return the location of said file.
+*/
+class Locator {
+
+    /*
+    * Assume that param fileLocation alwasy has the name of directory
+    */
+    public static String getFile(String fileLocation, String file) {
+        String folderIn = fileLocation;
+        File[] folder = new File(folderIn).listFiles();
+        int index = 0;
+
+        //System.out.println(folderIn);
+        
+        while(index < folder.length) {
+            if(folder[index].isFile()) {
+                if(file.equals(folder[index].getName())) {
+                    return folderIn + "/" + file;
+                }
+            }else {
+                folderIn = getFile(fileLocation + "/" + folder[index].getName(), file);
+
+                if(folderIn == null) {
+                    folderIn = fileLocation;
+                }else {
+                    return folderIn;
+                }
+            }
+
+            index++;
+        }
+
+        return null;
+    }
+}
--- Main.java
+++ Main.java
@@ -1,32 +1,6 @@
 class Main {
-    public static int recursionTest(int counter) {
-        //System.out.println(summers);
-        
-        if(counter >= 7) {
-            return counter - 0;
-        }
-
-        return recursionTest(counter + 1) - 1;
-    }
-    
     public static void main(String[] args) {
-        DoStuff n = new DoStuff();
-        MyStuff y = new MyStuff();
 
-        int[] arr = {5, 7, 2, 1, 9, 3};
-        
-        int j = 0;
-        int k = arr.length - 1;
-
-        while(j < k) {
-            int x = arr[j];
-            arr[j] = arr[k];
-            arr[k] = x;
-
-            j++;
-            k--;
-        }
-
-        System.out.println(arr[0]);
+        System.out.println(Locator.getFile("info", "bingo.txt"));
     }
 }
